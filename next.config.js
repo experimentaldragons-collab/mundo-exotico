921f9ac8615c57b9be2d7e904e4dac99d97e0986
experimentaldragons-collab <experimentaldragons@gmail.com>
2026-09-09 22:15:59 -0500

    chore: add next.config.js

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost', 'res.cloudinary.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
+  headers: async () => [
+    {
+      source: '/api/:path*',
+      headers: [
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff',
+        },
+        {
+          key: 'X-Frame-Options',
+          value: 'DENY',
+        },
+      ],
+    },
+  ],
+};
+
+module.exports = nextConfig;
